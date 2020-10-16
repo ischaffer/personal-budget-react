39caaff07139f95da406eda179b9b2731014b7f7
Indiana Schaffer <ischaffe@uncc.edu>
2020-10-16 11:39:15 -0400

    Added React

---
+++ src/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Footer() {
+  return (
+    <div className="footer">
+        <div class="center">
+            All rights reserved &copy; Fabio Nolasco
+        </div>
+    </div>
+  );
+}
+
+export default Footer;
